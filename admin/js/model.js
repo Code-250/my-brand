7751e6298dfcffcfa21a8249f6f0947f44b388e2
Code-250 <richmunye@gmail.com>
2021-12-24 19:11:32 +0200

    dashboard done

--- admin/js/model.js
+++ admin/js/model.js
@@ -1,14 +1,18 @@
 const model = document.querySelector("#my-model");
+const publish = document.querySelector(".submit-btn");
+const cancelPublish = document.querySelector(".cancel-btn");
 const editModel = document.querySelector("#my-edit-model");
 const createBlog = document.querySelector("#create-blog");
 const editBlog = document.querySelector("#edit-skill");
 const editBlog1 = document.querySelector("#edit-skill1");
 const editBlog2 = document.querySelector("#edit-skill2");
+const saveedit = document.querySelector(".submit-update-btn");
+const canceledit = document.querySelector(".cancel-update-btn");
 const close = document.getElementsByClassName("close")[0];
 const deleteBlog = document.querySelector("#my-delete-model");
-const deleteBtn = document.querySelector(".remove-skill");
-const deleteBtn1 = document.querySelector(".remove-skill1");
-const deleteBtn2 = document.querySelector(".remove-skill2");
+const deleteBtn = document.querySelector(".delete-blog");
+const deleteBtn1 = document.querySelector(".delete-blog1");
+const deleteBtn2 = document.querySelector(".delete-blog2");
 const comfirm = document.querySelector(".comfirm");
 const cancelDelete = document.querySelector(".cancel-delete");
 const skill = document.querySelector("#add-skill");
@@ -34,6 +38,12 @@ const cancelSkillDeleteBtn = document.querySelector(".cancel-skill-delete");
 
 createBlog.addEventListener("click", function () {
   model.style.display = "block";
+});
+publish.addEventListener("click", function () {
+  model.style.display = "none";
+});
+cancelPublish.addEventListener("click", function () {
+  model.style.display = "none";
 });
 close.onclick = function () {
   model.style.display = "none";
@@ -68,6 +78,12 @@ window.onclick = function () {
     editModel.style.display = "none";
   }
 };
+saveedit.addEventListener("click", function () {
+  editModel.style.display = "none";
+});
+canceledit.addEventListener("click", function () {
+  editModel.style.display = "none";
+});
 // delete a blog
 
 deleteBtn.addEventListener("click", function () {
