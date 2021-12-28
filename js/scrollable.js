972ead9b235b236de09ee24ecbc6bf6fc5460bcc
Code-250 <richmunye@gmail.com>
2021-12-29 00:43:49 +0200

    fixed all required changes

--- js/scrollable.js
+++ js/scrollable.js
@@ -1,9 +1,14 @@
 const sections = document.querySelectorAll("section");
 const scrollTopper = document.querySelector(".back-to-top");
 const scrollToAbout = document.querySelector(".view-work");
-const navLink = document.querySelectorAll(".nav-container ul a li");
+const navLink = document.querySelectorAll(".nav-container ul li");
 const mobileLinks = document.querySelectorAll(".nav-links-bars ul a li");
 const hambuger = document.querySelector(".fa-bars");
+const scrollToContact = document.querySelector(".contact-link");
+const scrollToBlogs = document.querySelector(".blog-link");
+const scrollToProjects = document.querySelector(".portfolio-link");
+const scrollAbout = document.querySelector(".about-link");
+const scrollHome = document.querySelector(".home-link");
 
 window.onscroll = () => {
   let current = "";
@@ -31,7 +36,21 @@ scrollTopper.addEventListener("click", () => {
 scrollToAbout.addEventListener("click", () => {
   window.scroll({ top: 1650, left: 0, behavior: "smooth" }), false;
 });
-
+scrollToContact.addEventListener("click", function () {
+  window.scroll({ top: 3600, left: 0, behavior: "smooth" });
+});
+scrollToBlogs.addEventListener("click", function () {
+  window.scroll({ top: 2350, left: 0, behavior: "smooth" });
+});
+scrollToProjects.addEventListener("click", function () {
+  window.scroll({ top: 1650, left: 0, behavior: "smooth" });
+});
+scrollAbout.addEventListener("click", function () {
+  window.scroll({ top: 750, left: 0, behavior: "smooth" });
+});
+scrollHome.addEventListener("click", function () {
+  window.scroll({ top: 0, left: 0, behavior: "smooth" });
+});
 hambuger.addEventListener("click", () => {
   const menu = document.querySelector(".nav-hidden");
   if (menu.style.display === "block") {
